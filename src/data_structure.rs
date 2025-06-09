5e9fe7d1daa7a682ed8f841a708d1ff9f6abe4c3
LiangChen3519 <61409749+LiangChen3519@users.noreply.github.com>
2025-06-09 16:51:51 +0300

    Revised the data structure

--- src/data_structure.rs
+++ src/data_structure.rs
@@ -1,9 +1,9 @@
 // this is a struct for storing spectrum data, like a big container
 #[derive(Debug)]
 pub struct SpectrumData {
-    pub id_data: String, // Frequency in Hz
+    pub id_data: String, // Id of the data
     pub datetime: String, // Date and time of the measurement
     pub temperature: String, // Temperature in Celsius
-    pub wavelength: Vec<f64>,
-    pub absorbance: Vec<f64>,
+    pub wavelength: Vec<f64>, // wavelength from 200 to 360 nm
+    pub absorbance: Vec<f64>, //..
 }
